562800cc392045884b11b6e1cde8e39546f9abfc
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-19 18:49:39 +0500

    Added Customer Controller and its view
    showing customers list on /Index
    getting the customers from DB

---
+++ RTLBootstrapTest/DataAccess/CustomerDataAcess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using RTLBootstrapTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTLBootstrapTest.DataAccess
+{
+    public class CustomerDataAcess
+    {
+        public List<CustomerModel> GetCustomers()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(@"Data Source =.\chinook.db; Version = 3;"))
+            {
+                var data = cnn.Query<CustomerModel>("SELECT * FROM Customers;").ToList();
+                return data;
+            }
+        }
+
+        public CustomerModel GetCustomer(int id)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(@"Data Source =.\chinook.db; Version = 3;"))
+            {
+                var data = cnn.Query<CustomerModel>($"SELECT * FROM Customers WHERE CustomerId = @ID;", new { ID = id }).SingleOrDefault();
+                return data;
+            }
+
+        }
+    }
+}
---
+++ RTLBootstrapTest/Models/CustomerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTLBootstrapTest.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Company { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String Country { get; set; }
+        public String PostalCode { get; set; }
+        public String Phone { get; set; }
+        public String Fax { get; set; }
+        public String Email { get; set; }
+        public int SupportedRepId { get; set; }
+    }
+}
